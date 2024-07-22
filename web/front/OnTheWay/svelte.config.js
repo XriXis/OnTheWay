454255380aae5cfabc5793b90473a32ae2c0e6d6
XriXis <i.lobazov@innopolis.university>
2024-07-22 08:28:50 +0300

    Attempts to deploy

--- web/front/OnTheWay/svelte.config.js
+++ web/front/OnTheWay/svelte.config.js
@@ -1,15 +1,11 @@
 import {vitePreprocess} from "@sveltejs/vite-plugin-svelte";
 import adapter from '@sveltejs/adapter-netlify';
 
-const base = '/app';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter(),
-    paths: {
-      base,
-    },
   },
   preprocess: vitePreprocess(),
   vitePlugin: {
